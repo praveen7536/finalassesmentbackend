649fe86327ed9190396cc52acf8dd538d8e9ff5c
Praveen Kumar <41022076+praveen7536@users.noreply.github.com>
2020-08-03 15:53:22 +0530

    second commit

---
+++ freshdeskapp/Migrations/20200803100935_ewd.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace freshdeskapp.Migrations
+{
+    public partial class ewd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
